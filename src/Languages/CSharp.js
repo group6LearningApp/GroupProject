88ee5f40e6b419a1849d917e79a46356d320bc66
BenReillyGit <55755383+BenReillyGit@users.noreply.github.com>
2019-11-26 19:35:36 +0000

    Add files via upload

---
+++ src/Languages/CSharp.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CSharp = () => (
+    <div>
+        <h2>C# Page</h2>
+    </div>
+)
---
+++ src/Languages/Python.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Python = () => (
+    <div>
+        <h2>Python Page</h2>
+    </div>
+)
