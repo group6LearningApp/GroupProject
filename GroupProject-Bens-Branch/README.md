84e292eeb53d8666f1c3501b2b5a0f3911ffd1d2
group6LearningApp <57458954+group6LearningApp@users.noreply.github.com>
2019-11-26 19:51:02 +0000

    Add files via upload

---
+++ GroupProject-Bens-Branch/README.md
@@ -0,0 +1,2 @@
+# GroupProject
+Learning App
---
+++ GroupProject-Bens-Branch/src/components/LanguageDrop.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Dropdown } from 'react-bootstrap';
+import './LanguageDrop.css';
+
+export const LanguageDrop = (props) => (
+    <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Languages
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+                <Dropdown.Item href="./Java">Java</Dropdown.Item>
+                <Dropdown.Item href="./Python">Python</Dropdown.Item>
+                <Dropdown.Item href="./CSharp">C#</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+)
