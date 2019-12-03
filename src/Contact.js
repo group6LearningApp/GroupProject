1ff7685de7606c38fdde964d2571df606f5c399d
Carl-Egan <carlegan1997@gmail.com>
2019-12-03 22:02:55 +0000

    Updated Contact.js
    Updated Contact.js to add a basic description of where to contact us and a link to the github repository

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,10 @@
 import React from 'react'
-
+import './App.css';
 export const Contact = () => (
     <div>
-        <h2>Contact Page</h2>
-    </div>
+      <h2 class="Languages" >Contact Page</h2>
+      <p class="Content">To make an suggestion , report bugs , or contact us feel free to email CS353.2019groupproject@gmail.com  </p>
+       <p class="Content">To view the source code and project folder for this app vist our github at the following 
+       <a target="_blank" href="https://github.com/group6LearningApp"> link</a></p>
+  </div>
 )
