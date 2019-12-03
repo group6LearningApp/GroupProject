8dca9df86ddee2ce050d86eecb15116c9e425e65
Carl-Egan <carlegan1997@gmail.com>
2019-12-03 21:04:21 +0000

    Updated the About page adding info and Carousel from bootstrap
    npm react-holder and npm react-bootstrap need to be performed to use carousel

--- src/About.js
+++ src/About.js
@@ -1,7 +1,50 @@
 import React from 'react'
-
+import Holder from 'react-holder';
+import Carousel from 'react-bootstrap/Carousel'
+import './App.css';
 export const About = () => (
-    <div>
-        <h2>About Page</h2>
+
+<div width='100%' class='Carousel'>
+<div width='100%' class='Carousel'>
+    <h1 class="Languages">CS353 Group Project</h1>
     </div>
+    <div width='100%'>
+<Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/933x456?text= &bg=#222"
+      alt=" "
+    />
+    <Carousel.Caption>
+      <h1 class="Languages" >About</h1>
+      <p>This is a web page developed by 3rd year Computer Science Software Engineer students from Maynooth University</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/933x456?text= &bg=222"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h2 class="Languages" >About</h2>
+      <p>The purpose of this page is to learn and teach people coding of all levels and skillsets</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/933x456?text= &bg=222"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h2 class="Languages" >About</h2>
+      <p>This project was created by Ben Reilly , David Afalla , Mariia Skyba , Carl Egan and Shane Dunne </p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel></div>
+</div>
 )
