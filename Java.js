cd084bf420988ab16e5ad723bec3befe45e7d34a
Mariia Skyba <mariia.skyba.2018@mumail.ie>
2019-12-03 21:45:40 +0000

    Update Java.js

--- Java.js
+++ Java.js
@@ -1,6 +1,6 @@
-//This is a modified version of Ben's Java.js file.
-//It contains an "Ask a Question" Form appearing when
-//you click onto "Java" topic in the dropdown menu
+//This is a modified version of Ben's Java.js file. It contains an "Ask a Question" Form appearing when
+//you click onto "Java" topic in the dropdown menu. If you fill in the fields in the form and click "Ask a question" button - 
+//the data you entered will be stored in Firebase
 
 import React, { Component } from 'react';
 import fire from '../config/Fire';
@@ -9,7 +9,9 @@ export default class Java extends Component {
         super();
         this.state = {
          lastname: "",
-         firstname: ""
+         firstname: "",
+         title: "",
+         content:""
         };
     }
     
@@ -35,20 +37,27 @@ export default class Java extends Component {
         });
       };
 
-      showAQForm = e => {
-        var state = document.getElementById("AQForm").style.display;
-        if(state != "none"){
-          document.getElementById("AQForm").style.display = "none";
-        }
-        else{
-          document.getElementById("AQForm").style.display = "block";
-        }
-      }
+      submitQuestion = e => {
+        e.preventDefault();
+        const db = fire.firestore();
+        db.settings({
+        timestampsInSnapshots: true
+        });
+        const userRef = db.collection("Questions").add({
+        title: this.state.title,
+        content: this.state.content,
+        attachment: null
+        });  
+        this.setState({
+          fullname: "",
+          email: ""
+        });
+      };
 
     render() {
         return (
         <div>
-            {/* <form onSubmit={this.addUser}>
+             {/*<form onSubmit={this.addUser}>
                 <input
                 type="text"
                 name="firstname"
@@ -64,33 +73,35 @@ export default class Java extends Component {
                 value={this.state.lastname}
                 />
                 <button type="submit">Submit</button>
-          </form> */}
+        </form> */}
           <div className="wrapper1">
         <div id = "AQForm" className = "form-wrapper1" display = "none">
         {/* Title field */}
         <h1>Ask a question</h1>
-        <form onSubmit={this.handleSubmit} noValidate>
+        <form onSubmit={this.submitQuestion}>
             <div className="title">
                 <label htmlFor="title">Title</label>
                 <input 
                 type="text" 
                 className="title" 
-                placeholder="Title" 
+                placeholder="Title"
+                onChange={this.updateInput} 
                 name="title" 
                 noValidate
-                onChange={this.handleChange}
+                //onChange={this.handleChange}
                 />
             </div>
             {/* Question body field */}
-            <div className="qBody">
-                <label htmlFor="qBody">Question</label>
-                <input 
+            <div className="content">
+                <label htmlFor="content">Question</label>
+                <textarea 
                 type="text" 
-                className="qBody" 
+                className="content" 
                 placeholder="Type your question here" 
-                name="qBody" 
+                onChange={this.updateInput}
+                name="content" 
                 noValidate
-                onChange={this.handleChange}
+                //onChange={this.handleChange}
                 />
             </div>
             {/* Attachment/upload a photo field */}
@@ -101,7 +112,7 @@ export default class Java extends Component {
                     type="file" 
                     accept="image/*" 
                     noValidate
-                    onChange={this.handleChange}
+                    //onChange={this.handleChange}
                 />
             </div>
             {/* Submit a question button */}
@@ -109,9 +120,10 @@ export default class Java extends Component {
 
             </form>
     </div>
-  </div>
+   </div>
           <p>{this.state.firstname}</p>
-          <button class = "create" onclick = 'showAQForm()'>Ask a question</button>
+          <button class = "create" onclick = "this.askQuestion">Ask a question</button>
+          <button class = "create" onclick = "this.askQuestion">Post a resource</button>
         
         </div>
           
