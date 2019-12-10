a869b8b575d472d0f0b5d2482f79c55660c6be70
Mariia Skyba <mariia.skyba.2018@mumail.ie>
2019-12-10 22:47:01 +0000

    Updated AskQuestionForm.js

--- AskQuestionForm.js
+++ AskQuestionForm.js
@@ -1,55 +1,101 @@
+//AskQuestionForm.js
+//Mariia Skyba
+//This is an updated "Ask a Queston" form which gets called by "Ask a Question" button located on the Java Language main page
+//This form is imported into Java.js file 
+
 import React, { Component } from 'react';
-import './App.css';
-//login in -> authorise ->
+import '../App.css';
+import fire from '../config/Fire'; 
+
 class AskQuestionForm extends Component{
+
+  constructor() {
+    super();
+    this.state = {
+      //class attributes (fields in the AskQuestionForm)
+      title: "",
+      content:"",
+      attachment: ""
+    };
+  }
+  
+  //?
+  updateInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  //function to save input from "Ask a Question" Form into Firebase database
+  submitQuestion = e => {
+      e.preventDefault();
+      const db = fire.firestore();
+      db.settings({
+            timestampsInSnapshots: true
+      });
+      const userRef = db.collection("Questions").add({ //add to collection called "Questions"
+          title: this.state.title, //store question title
+          content: this.state.content, //store question content
+          attachment: null //don't store anything in the attachment field yet as we don't know if storing images in Firebase is going to work
+      });  
+  };
+
+
   render(){
     return( 
     <React.Fragment>
-    
+   <div>
+    {/* Ask Question Form */}
     <div className="wrapper1">
-        <div className = "form-wrapper1">
+      <div className = "form-wrapper1">
+
         <h1>Ask a question</h1>
-        <form onSubmit={this.handleSubmit} noValidate>
+        <form onSubmit={this.submitQuestion}>
+
+          {/* Title field */}
             <div className="title">
-                <label htmlFor="title">Title</label>
-                <input 
+              <label htmlFor="title">Title</label>
+              <input 
                 type="text" 
                 className="title" 
-                placeholder="Title" 
+                placeholder="Title"
+                onChange={this.updateInput} 
                 name="title" 
                 noValidate
-                onChange={this.handleChange}
-                />
+              />
             </div>
 
-            <div className="qBody">
-                <label htmlFor="qBody">Question</label>
-                <input 
+            {/* Question content field */}
+            <div className="content">
+              <label htmlFor="content">Question</label>
+              <textarea
                 type="text" 
-                className="qBody" 
-                placeholder="Type your question here" 
-                name="qBody" 
+                className="content" 
+                placeholder="Type your question here"
+                value={this.state.content} 
+                onChange={this.updateInput}
+                name="content" 
                 noValidate
-                onChange={this.handleChange}
-                />
+              />
             </div>
 
+            {/* Attachment field */}
             <div className="attachment">
-                <label htmlFor="attachment">Upload a photo</label>
+              <label htmlFor="attachment">Upload a photo</label>
                 <input 
-                    id="uploadFile"
-                    type="file" 
-                    accept="image/*" 
-                    noValidate
-                    onChange={this.handleChange}
+                  id="uploadFile"
+                  type="file" 
+                  accept="image/*" 
+                  noValidate
                 />
             </div>
 
-            <button className="create">ASK A QUESTION</button>
-
-            </form>
-    </div>
-  </div>
+            {/* Submit a question button */}
+            <button type="submit" className="create">ASK A QUESTION</button>          
+          </form>
+        </div>
+      </div>
+    </div>    
   
   </React.Fragment>
     
