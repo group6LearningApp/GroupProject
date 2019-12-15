b4e517d26f8c1d0baef93814bc10126ac751d91e
group6LearningApp <57458954+group6LearningApp@users.noreply.github.com>
2019-12-15 01:48:41 +0000

    Basic Page, Unfinished
    Needs to take in doc data and comments

---
+++ PostPage.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class PostPage extends Component {
+    /*constructor(props) {
+        super(props);
+        this.state = {
+            Info: {}
+        }
+    }*/
+    render() {
+        return (
+            <div>
+                <h2>Hello :)</h2>
+            </div>
+        )
+    }
+}
+
+/*export const PostPage = () => (
+    <div>
+        <h2>Did it work?</h2>
+    </div>
+)*/
