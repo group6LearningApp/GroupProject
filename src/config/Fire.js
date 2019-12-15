72dfad0dc778bd729ea7a06af94b44842c08ab68
Carl-Egan <45272780+Carl-Egan@users.noreply.github.com>
2019-12-15 20:46:20 +0000

    Add files via upload
    changed config to export db and auth to be used in topPosts

--- src/config/Fire.js
+++ src/config/Fire.js
@@ -2,20 +2,22 @@ import firebase from 'firebase';
 
 
 
-var firebaseConfig = {
-  apiKey: "AIzaSyBg1Y49CWrE0M9jO3brU9XDSqe54OF75Dk",
-    authDomain: "benstest-fc98a.firebaseapp.com",
-    databaseURL: "https://benstest-fc98a.firebaseio.com",
-    projectId: "benstest-fc98a",
-    storageBucket: "benstest-fc98a.appspot.com",
-    messagingSenderId: "127013225279",
-    appId: "1:127013225279:web:e71561e272cd7c4381c98a",
-    measurementId: "G-73SPTRE8PP"
-
-
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyB3lPsP7OcWw8t1IgAEAizXYSvIU_W0BaA",
+  authDomain: "learningapp-dce88.firebaseapp.com",
+  databaseURL: "https://learningapp-dce88.firebaseio.com",
+  projectId: "learningapp-dce88",
+  storageBucket: "learningapp-dce88.appspot.com",
+  messagingSenderId: "265370756402",
+  appId: "1:265370756402:web:1170b2d0e9cf8cc785a32b",
+  measurementId: "G-S3Q5SDEKZN"
+};
   // Initialize Firebase
   const fire = firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-  export default fire;
- 
+  // firebase.analytics();
+  // export default fire;
+  export const auth = firebase.auth()
+  export const db = firebase.firestore()
+  
+
+  export default firebase
