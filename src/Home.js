354a601b7374d7e4e1d499abf13fb61178b192e7
BenReillyGit <55755383+BenReillyGit@users.noreply.github.com>
2019-12-16 13:09:57 +0000

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -1,53 +1,74 @@
 import React, { Component } from 'react'
-import  TopPosts from './TopPosts';
+import TopPosts  from './components/TopPosts';
 import { LanguageDrop } from './components/LanguageDrop';
 import fire from './config/Fire';
 import './Home.css';
 
-
 class Home extends Component {
     constructor(props) {
         super(props);
-        this.logout = this.logout.bind(this);
+        
         this.state = {
             user:{},
-            email:''
+            email:'',
+            users: {},
+            key: ''
         }
     }
-   
-
-    logout() {
-        fire.auth().signOut();
-    }
-
     componentDidMount() {
-        this.authListener();
+      this.authListener();
+        fire.auth().onAuthStateChanged((user) => {
+          if(user) {
+            this.setState({ user });
+        var ref = fire.firestore().collection('users').doc(this.state.user.uid);
+        ref.get().then((doc) => {
+          if(doc.exists) {
+            console.log(doc.data());
+            this.setState({
+              
+              users: doc.data(),
+              key: doc.id
+              
+            })
+          }
+        })
+      } 
+      }); 
       }
-      
-
       authListener() {
         fire.auth().onAuthStateChanged((user) => {
           console.log(user);
           if(user) {
             this.setState({ user });
-           
-          } 
+          } else {
+            this.setState({ user: null });
+          }
         });
       }
-
       
-
-    render() {
+    render() {     
         return (
-    <div>
-      
-        
-        <h1>Hi {this.state.user.email}</h1>
-        <h2>Learning App</h2>
-        <p>Please select language of choice</p>
-        <LanguageDrop />
-        <TopPosts />    
-    </div>
+        this.state.user ? (
+          <div>
+          <br />
+          <h1>Hello {this.state.users.firstname} {this.state.users.lastname}</h1>
+          <h2>Learning App</h2>
+          <p>Please select language of choice</p>
+          <LanguageDrop />
+          <TopPosts />
+          <br />
+          </div>   
+          ) : (
+          <div>
+          <h1>Hi Uregistered User! If you want to write posts please create an account or login.</h1>
+          <h2>Learning App</h2>
+          <p>Please select language of choice</p>
+          <LanguageDrop />
+          <TopPosts />
+          <br />
+          </div>
+          )
+    
         );
     } 
 }
